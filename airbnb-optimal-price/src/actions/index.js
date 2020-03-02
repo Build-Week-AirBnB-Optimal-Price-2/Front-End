20d620e3eab18df931fcde6ef26c54fc16e0e4d9
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-02 15:06:10 -0800

    setting up redux store, reducers, etc

---
+++ airbnb-optimal-price/src/actions/index.js
@@ -0,0 +1,18 @@
+import axiosWithAuth from '../utils/axiosWithAuth'
+
+export const FETCH_DATA = 'FETCH_DATA';
+export const UPDATE_PROPERTIES = 'UPDATE_PROPERTIES'
+export const SET_ERROR = 'SET_ERROR';
+
+export const getData = userId => dispatch => {
+    dispatch({ type: FETCH_DATA });
+    axiosWithAuth()
+        .get(/* use address to backend */)
+        .then( res => {
+            dispatch({ type: UPDATE_PROPERTIES, payload: res.data });
+        })
+        .catch( error => {
+            console.error('error fetching data from api, error: ', error);
+            dispatch({ type: SET_ERROR, payload: 'Error fetching data from api'})
+        })
+}
