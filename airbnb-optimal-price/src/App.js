c933b9a79d4327d2d5519a4b891b28fd750ed9ab
isaac stott <isaac.cloyd@gmail.com>
2020-03-03 09:55:48 -0700

    merge process

--- airbnb-optimal-price/src/App.js
+++ airbnb-optimal-price/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import LoginForm from './loginPage';
+import LoginForm from './components/LoginPage';
 
 function App() {
   return (
--- airbnb-optimal-price/src/components/LoginForm.js
+++ airbnb-optimal-price/src/components/LoginForm.js
@@ -5,3 +5,64 @@
 //username
 //password
 //submit button
+
+import React, {useState} from 'react';
+
+
+
+const NoteForm = props => {
+    // console.log("props", props);
+    const [note, setNote] = useState({
+      email: "",
+      password: "",
+    });
+  
+    const handleChanges = e => {
+    //   console.log(note);
+    
+      setNote({
+        ...note,
+        [e.target.name]: e.target.value
+      });
+    };
+
+  
+    const submitForm = e => {
+      e.preventDefault();
+      props.login(note);
+      setNote({ email: "", password: ""});
+    };
+  
+    return (
+      <form onSubmit={submitForm}>
+        <div className="formdiv">
+          <div className="namestuff">
+            <label htmlFor="email">Email </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              onChange={handleChanges}
+              value={note.email}
+            />
+          </div>
+          
+          <div className="namestuff">
+            <label htmlFor="password">Password </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={handleChanges}
+              value={note.password}
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    );
+};
+  
+export default NoteForm;
+  
